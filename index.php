c6e41ef2aa77f1757fed8ad0e41faaf47204d950
M35hugge <150009524+M35hugge@users.noreply.github.com>
2024-07-08 11:42:27 +0200

    Index template added

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+require("includes/common.inc.php");
+require("includes/db.inc.php");
+?>
+<!DOCTYPE html>
+<html lang="de-DE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Pool Roboter</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<h2>Index</h2>
+<nav>
+    <a href="#">Home</a>
+    <a href="#">Link1</a>
+    <a href="#">Link2</a>
+</nav>
+</body>
+</html>
